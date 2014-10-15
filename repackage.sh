dcc8e37186ec3878444b7dc84fcb67e4a7c7f440
Security Generation <info@securethoughts.net>
2014-10-15 14:10:24 +0100

    Minor package update

--- repackage.sh
+++ repackage.sh
@@ -1,2 +1,2 @@
 # This script just creates a new dome9.spk
-rm dome9.spk; rm package.tgz;tar -cvzf package.tgz dome9;tar -cvf dome9.spk INFO WIZARD_UIFILES/ package.tgz scripts/ PACKAGE_ICON.PNG
+rm dome9.spk;rm package.tgz;tar -cvzf package.tgz dome9;tar -cvf dome9.spk INFO WIZARD_UIFILES/ package.tgz scripts/ PACKAGE_ICON.PNG
